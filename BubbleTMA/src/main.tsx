173f27b4b4d8b08b17b8cbe8d5bfba7cccfd3148
Kwed <159473699+Kwed1@users.noreply.github.com>
2025-01-01 18:01:23 +0400

    ton

--- BubbleTMA/src/main.tsx
+++ BubbleTMA/src/main.tsx
@@ -1,3 +1,4 @@
+import { THEME, TonConnectUIProvider } from '@tonconnect/ui-react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
@@ -7,8 +8,12 @@ import store from './store/index.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <Router />
-    </Provider>
+    <TonConnectUIProvider
+      manifestUrl={`https://tgmochispa.devmainops.store/tonconnect-manifest.json`}
+      uiPreferences={{ theme: THEME.DARK }}>
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    </TonConnectUIProvider>
   </StrictMode>,
 )
--- BubbleTMA/src/sections/Home/index.tsx
+++ BubbleTMA/src/sections/Home/index.tsx
@@ -1,10 +1,38 @@
+import { useTonAddress, useTonConnectUI } from '@tonconnect/ui-react'
 import { useNavigate } from 'react-router-dom'
 import styles from './style.module.css'
+
 export function Home() {
-	const navigate = useNavigate()
-	return (
-		<div className={styles?.menu}>
-			<button onClick={() => navigate('/game')}>Go to game</button>
-		</div>
-	)
+  const navigate = useNavigate()
+	const [tonConnectUI] = useTonConnectUI();
+  const userFriendlyAddress = useTonAddress();
+
+ const handleConnectWallet = async () => {
+    try {
+      await tonConnectUI.connectWallet();
+      console.log("Wallet connected successfully");
+    } catch (error) {
+      console.error("Error connecting wallet:", error);
+    }
+  };
+
+  const handleDisconnectWallet = async () => {
+    try {
+      await tonConnectUI.disconnect();
+      console.log("Wallet disconnected successfully");
+    } catch (error) {
+      console.error("Error disconnecting wallet:", error);
+    }
+  };
+
+  return (
+    <div className={styles?.menu}>
+      <button onClick={() => navigate('/game')}>Go to game</button>
+      <button onClick={() => handleConnectWallet()}>Connect TON wallet</button>
+			{userFriendlyAddress && (
+				<button onClick={handleDisconnectWallet}>Disconnect TON wallet</button>
+			)}
+			{userFriendlyAddress}
+    </div>
+  )
 }
