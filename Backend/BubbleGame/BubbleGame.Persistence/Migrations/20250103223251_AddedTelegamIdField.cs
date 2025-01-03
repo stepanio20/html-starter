8f3a2e75044c81efd2f1942b9506a33188289958
lcky <lcky>
2025-01-04 00:48:30 +0100

    bug fix

---
+++ Backend/BubbleGame/BubbleGame.Persistence/Migrations/20250103223251_AddedTelegamIdField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BubbleGame.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTelegamIdField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TelegramId",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TelegramId",
+                table: "AspNetUsers");
+        }
+    }
+}
