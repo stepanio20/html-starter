b2006e0cf2d88bcc915a5f594969e16869ccbfcb
Kwed <159473699+Kwed1@users.noreply.github.com>
2025-01-04 21:46:13 +0400

    fixed

--- Backend/BubbleGame/BubbleGame.Domain/obj/Debug/net9.0/BubbleGame.Domain.AssemblyInfo.cs
+++ Backend/BubbleGame/BubbleGame.Domain/obj/Debug/net9.0/BubbleGame.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BubbleGame.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3795db569d95d73c3d5a8f0cf6611f646267524")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a571814357999256797d620827461167cee8af")]
 [assembly: System.Reflection.AssemblyProductAttribute("BubbleGame.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BubbleGame.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
