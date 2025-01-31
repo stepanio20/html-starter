49ed18d9886bc88255dfacb7b07ebdb1408079d3
lcky <lcky>
2025-01-31 20:00:07 +0100

    test

--- Backend/BubbleGame/BubbleGame.Domain/DustParticles/DustParticle.cs
+++ Backend/BubbleGame/BubbleGame.Domain/DustParticles/DustParticle.cs
@@ -4,7 +4,7 @@ namespace BubbleGame.Core.DustParticles;
 
 public class DustParticle : CacheEntity
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; init; } = Guid.NewGuid();
     public float PositionX { get; set; }
     public float PositionY { get; set; }
     public float Size { get; set; }
